68eac28ca62462cf30a1df3d735e05f26a678296
fresino <mrym.124.hys@gmail.com>
2024-06-22 05:14:34 +0000

    [finish]Creating book_show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,25 @@ class BooksController < ApplicationController
   end
 
   def index
-    @book = Book.new
     @books = Book.all
+    @book = Book.new
   end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "/homes/about" => "homes#about", as: "about"
   devise_for :users
-  
-  resources :books, only: [:new, :create, :index ,:show, :destroy]
+
+  resources :books, only: [:new, :create, :index ,:show, :destroy, :edit]
   resources :users, only: [:show, :edit, :update, :index]
 end
