75def26eb1181c0c9be474fd5de1f063df2a02da
fresino <mrym.124.hys@gmail.com>
2024-06-23 07:16:53 +0000

    [start]RSpec_1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,12 @@ class UsersController < ApplicationController
   end
 
   def edit
+    is_matching_login_user
     @user = User.find(params[:id])
   end
 
   def update
+    is_matching_login_user
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "You have updated user successfully."
@@ -20,14 +22,22 @@ class UsersController < ApplicationController
     end
   end
 
-
   def index
     @book = Book.new
     @users = User.all
   end
 
+private
+
   def user_params
     params.require(:user).permit(:name,:profile_image, :introduction)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
   end
 
 end
