bea93f9e2eda965ef89a8168265e1aaf6ef342e1
fresino <mrym.124.hys@gmail.com>
2024-06-24 09:44:39 +0000

    [finish]Task4

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :books, only: [:new, :create, :index ,:show, :destroy, :edit, :update] do
-    resources :favorites, only: [:create]
-    resource :favorite, only: [:destroy]
+    resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update, :index]
